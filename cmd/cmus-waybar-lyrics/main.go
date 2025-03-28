62ad853531dc283d2aa4d0648844a061216acc6b
Guilherme <guipuszkarek@gmail.com>
2025-03-28 12:33:07 -0300

    avoid reading path every time

--- cmd/cmus-waybar-lyrics/main.go
+++ cmd/cmus-waybar-lyrics/main.go
@@ -10,32 +10,54 @@ import (
 
 	"github.com/puszkarek/cmus-waybar-lyrics/internal/cmus"
 	"github.com/puszkarek/cmus-waybar-lyrics/internal/lyrics"
+	"github.com/puszkarek/cmus-waybar-lyrics/internal/models"
 	"github.com/puszkarek/cmus-waybar-lyrics/internal/utils"
 	"github.com/puszkarek/cmus-waybar-lyrics/internal/waybar"
 )
 
-func update() {
+func main() {
+	// Handle SIGUSR1 for manual updates
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGUSR1)
+
+	// Variables to track the current song
+	var currentSongPath string
+	var currentSong models.SongMetadata
+
+	// Function to update lyrics
+	updateLyrics := func() {
 		songStatus, err := cmus.GetSongStatusFromCMUS()
 		if err != nil {
 			// It's almost sure not running
 			waybar.RenderNoSong()
-			time.Sleep(time.Second *2)
-			return
-		}
-		song, err := lyrics.GetFullLyrics(songStatus.Path)
-		if err != nil {
-			waybar.RenderNoSong()
-			fmt.Println("Error getting lyrics:", err)
 			time.Sleep(time.Second * 2)
 			return
 		}
 
-		if utils.IsEmptyString(song.Lyrics) && utils.IsNotEmptyString(song.Artist) && utils.IsNotEmptyString(song.Title) {
-			waybar.RenderSongStatus(song)
+		// Check if the song has changed
+		if songStatus.Path != currentSongPath {
+			// Song has changed, fetch new lyrics
+			song, err := lyrics.GetFullLyrics(songStatus.Path)
+			if err != nil {
+				waybar.RenderNoSong()
+				fmt.Println("Error getting lyrics:", err)
+				time.Sleep(time.Second * 2)
+				return
+			}
+			
+			// Update current song info
+			fmt.Println("Song changed, updating waybar...")
+			currentSongPath = songStatus.Path
+			currentSong = song
+		}
+
+		if utils.IsEmptyString(currentSong.Lyrics) && utils.IsNotEmptyString(currentSong.Artist) && utils.IsNotEmptyString(currentSong.Title) {
+			waybar.RenderSongStatus(currentSong)
+			time.Sleep(time.Second * 2)
 			return
 		}
 
-		lyricsStatus, err := lyrics.GetCurrentLyricsLine(song.Lyrics, songStatus.Duration, songStatus.Position)
+		lyricsStatus, err := lyrics.GetCurrentLyricsLine(currentSong.Lyrics, songStatus.Duration, songStatus.Position)
 		if err != nil {
 			fmt.Println("Error getting current lyrics line:", err)
 			time.Sleep(time.Second * 2)
@@ -43,7 +65,7 @@ func update() {
 		}
 
 		// Get the time until the next line should be displayed
-		 waybar.RenderLyrics(lyricsStatus)
+		waybar.RenderLyrics(lyricsStatus)
 
 		// If we know when the next line should appear, sleep until then
 		if lyricsStatus.TimeToNext > 0 {
@@ -53,29 +75,20 @@ func update() {
 			// Otherwise check every second
 			time.Sleep(time.Second)
 		}
+	}
 
-}
-
-
-
-func main() {
-		// Handle SIGUSR1 for manual updates
-		sigChan := make(chan os.Signal, 1)
-		signal.Notify(sigChan, syscall.SIGUSR1)
-
-		go func() {
-			for range sigChan {
-				fmt.Println("Received SIGUSR1, updating waybar...")
-				update()
-			}
-		}()
-
-// TODO: Find a way to only update the song metadata when the song changes
-// TODO: Find a way to only update the lyrics when the song changes
-// TODO: Improve synchronization between song and lyrics updates
-
-		for {
-		update()			
+	// Handle SIGUSR1 signal for manual updates
+	go func() {
+		for range sigChan {
+			fmt.Println("Received SIGUSR1, updating waybar...")
+			// Force a refresh of lyrics on signal
+			currentSongPath = "" // Reset the path to force refresh
+			updateLyrics()
 		}
+	}()
 
+	// Main loop
+	for {
+		updateLyrics()
+	}
 }
